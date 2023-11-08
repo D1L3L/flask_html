a6ad39fc43219557eb30a89ee94fd8d310e6d740
Dani Oliveira <86899014+D1L3L@users.noreply.github.com>
2023-11-08 18:10:34 -0300

    adicionando css a pasta static

--- app.py
+++ app.py
@@ -2,23 +2,18 @@ from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
-def index():
-    return render_template('index.html')
-
 def executa_insert(consultadb):
-    con= conecta_db()
+    con = conecta_db()
     cursor = con.cursor()
     cursor.execute(consultadb)
     con.commit() # Envia as variáveis para o db
     con.close() 
     
-    dados = str("insert into public.contato(nome, telefone, email, senha) values('"+nome+","+email+","+senha+"')")
-    executa_insert(dados)
-  
 def conecta_db():
-  con = psycopg2.connect(host = 'localhost', database = 'dbbarbeiro', user = "postgres",  password= "postgres", port = 5432)
+  con = psycopg2.connect(host = 'localhost', database = 'dbbarbeiro', user = "postgres",  password= "postgres", port = 5432
+      )
   return con
-    
+  
 @app.route('/')
 def login():
     return render_template('index.html')
@@ -27,18 +22,21 @@ def login():
 def cadastro():
     return render_template('cadastro.html')
     
-@app.route('/fazer-login', methods=['POST'])
+@app.route('/fazer-login', methods=['GET'])
 def fazerLogin():
-    user = request.form.get('email')
+    email = request.form.get('email')
     senha = request.form.get('senha')
     
 @app.route('/fazer-cadastro', methods=['POST'])
 def fazercadastro():
-    user = request.form.get('email')
+    email = request.form.get('email')
     senha = request.form.get('senha')
     nome = request.form.get('nome')
-    def executa_insert():
-        return render_template('index.html')
+    if len(senha) < 3:
+        return render_template('cadastro.html', message="Senha muito curta")
+    dados = str("insert into public.form(nome, email, senha) values('"+nome+"','"+email+"','"+senha+"')")
+    executa_insert(dados)  
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
